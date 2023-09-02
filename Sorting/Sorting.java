934d13b3362eda9d0ed39b9b9fac80f7e4238d5e
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-02 11:09:38 +0530

    INsertion and Bubble Sort Done

---
+++ Sorting/Sorting.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Sorting {
+
+    // Take the element and compar its left
+    static int[] insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+        return arr;
+    }
+
+    // Push the max element to the right of the array by adjacent swapping
+    static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
