616366c4e0eef694fd7412e77a9d8230302b227b
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-02 11:26:21 +0530

    Done with basic sorting

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -31,13 +31,30 @@ class Sorting {
         return arr;
     }
 
+    // find in the unsorted array and swap with the first element of the unsorted
+    // array
+    static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        bubbleSort(arr);
+        selectionSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i] + " ");
         }
