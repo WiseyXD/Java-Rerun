7270f915da9eebc9880605be3924842220606440
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-19 11:35:56 +0530

    HashSet and HashMap to be done

---
+++ HashMap and HashSet/Hash.java
@@ -0,0 +1,12 @@
+package HashMap
+
+and HashSet;
+
+import java.util.*;
+
+public class Hash {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        
+    }
+}
